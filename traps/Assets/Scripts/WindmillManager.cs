4e4967d0a012417d3d6f4eff89f5576ca0e6b605
Floayza <fabianloayzarivera@gmail.com>
2017-12-14 15:20:32 +0100

    Added windmill

---
+++ traps/Assets/Scripts/WindmillManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillManager : MonoBehaviour {
+
+	public GameObject windmillHead;
+	public float rotationSpeed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		windmillHead.transform.Rotate(0,rotationSpeed * Time.deltaTime,0);
+	}
+}
